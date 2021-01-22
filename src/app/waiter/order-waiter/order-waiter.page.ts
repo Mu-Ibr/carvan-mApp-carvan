20ac17a75a972d10b5f9204b701fd597a7f9deb5
Mu-Ibr <mmi.8822@hotmail.com>
2021-01-22 17:05:36 +0200

    Updated the Calendar section

--- src/app/waiter/order-waiter/order-waiter.page.ts
+++ src/app/waiter/order-waiter/order-waiter.page.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { MenuController } from '@ionic/angular';
 import { OrderService } from 'src/app/order.service';
 
 
